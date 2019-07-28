51339a1efe6e537bea34a7fc054af507cf401ed0
Wu Yuhang <53282395+WYHIII@users.noreply.github.com>
2019-07-28 16:34:41 +0800

    VERSION 1.0.0

--- main.cpp
+++ main.cpp
@@ -15,21 +15,31 @@
 
 #define VERSION "0.1.0 (INSIDER)"
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::queue;
+using std::putchar;
+using std::setw;
+
+void start();
+void welcome();
+void draw();
+void play();
+inline void gameover();
+
+void win();
+bool check(int, int);
+void open(int, int);
+void flag(int, int);
+void del(int, int);
 
 const int MAXN = 100 + 3;
 const int dx[] = {-1, 0, 1, 1, 1, 0, -1, -1};
 const int dy[] = {1, 1, 1, 0, -1, -1, -1, 0};
 
 bool m[MAXN][MAXN], vis[MAXN][MAXN], f[MAXN][MAXN];
-int num[MAXN][MAXN], level, n, size;
-
-void start();
-void welcome();
-void draw();
-void play();
-
-//int scan(const string&);
+int num[MAXN][MAXN], level, n, size, flags;
 
 int main() {
     start();
@@ -56,6 +66,18 @@ void start() {
         cnt++;
     }
 
+    for (int i = 1; i <= size; i++) {
+        for (int j = 1; j <= size; j++) {
+            if (!m[i][j]) {
+                for (int k = 0; k < 8; k++) {
+                    int xx = i + dx[k];
+                    int yy = j + dy[k];
+                    num[i][j] += m[xx][yy] ? 1 : 0;
+                }
+            }
+        }
+    }
+
     draw();
 }
 
@@ -69,7 +91,8 @@ void draw() {
     for (int i = 1; i <= size; i++) {
         cout << setw(3) << i; putchar('|');
         for (int j = 1; j <= size; j++) {
-            if (!vis[i][j]) cout << " * ";
+            if (f[i][j]) cout << " ! ";
+            else if (!vis[i][j]) cout << " * ";
             else cout << ' ' << num[i][j] << ' ';
         }
         cout << endl;
@@ -77,23 +100,9 @@ void draw() {
     cout << "   |" << endl << "   Y" << endl << "If you want to open a cell, input like this:'o 5 9'" << endl << "if you want to flag a cell, input like this:'f 5 9'" << endl << "if you want to cancle what you've done, input like this:'d 5 9'" << endl << endl;
 }
 
-
-void play() {
-    char c;
-    int x, y;
-
-    cin >> c;
-    switch(c) {
-        case 'o': cin >> x >> y; if (m[x][y]) {system("cls"); cout << "Game Over :(";} else {vis[x][y] = true; draw();} break;
-        case 'f': cin >> x >> y; f[x][y] = true; draw(); break;
-        case 'd': cin >> x >> y; f[x][y] = false; draw(); break;
-    }
-
-}
-
 void welcome() {
     cout << "Welcome to Minesweeper " << VERSION << "." << endl << "Press any key to continue."; getch(); system("cls");
-    cout << "Select difficult level." << endl << "Level 1: 10 mines , 10 * 10 (EASY)" << endl << "Level 2: 40 mines , 20 * 20 (MEDIUM)" << endl << "Level 3: 100 mines , 50 * 50 (DIFFICULT)" << endl;
+    cout << "Select difficult level." << endl << "Level 1: 20 mines , 10 * 10 (EASY)" << endl << "Level 2: 80 mines , 20 * 20 (MEDIUM)" << endl << "Level 3: 500 mines , 50 * 50 (DIFFICULT)" << endl;
     char c; cin >> c;
     while (c != '1' && c != '2' && c != '3') {
         cout << "Wrong number, please input again." << endl;
@@ -102,8 +111,61 @@ void welcome() {
     level = c - '0';
     system("cls");cout << "Press any key to start the Game"; getch(); system("cls");
 
-    if (level == 1) {n = 10; size = 10;}
-    else if (level == 2) {n = 40; size = 20;}
-    else if (level == 3) {n = 100; size = 50;}
-    else abort();
+    if (level == 1) {n = flags = 20; size = 10;}
+    else if (level == 2) {n = flags = 80; size = 20;}
+    else if (level == 3) {n = flags = 500; size = 50;}
+    else exit(1);
+}
+
+inline void gameover() {
+    system("cls"); cout << "Game Over :(" << endl;
+    system("pause"); exit(0);
+}
+
+bool check(int x, int y) {
+    if (x > 0 && x <= size && y > 0 && y <= size) return true;
+    cout << "Illegal instruction." << endl << "Press any key to continue." << endl; getch();
+    return false;
+}
+
+void win() {
+    for (int i = 1; i <= size; i++)
+        for (int j = 1; j <= size; j++)
+            if (m[i][j] && !f[i][j]) return;
+
+    cout << "You win :)" << endl << "Press any key to continue." << endl; getch(); exit(0);
+}
+
+void play() {
+    char c; int x, y;
+
+    while (cin >> c) {
+        switch(c) {
+            case 'o': case 'O': cin >> y >> x; if (check(x, y)) open(x, y); break;
+            case 'f': case 'F': cin >> y >> x; if (check(x, y)) flag(x, y); break;
+            case 'd': case 'D': cin >> y >> x; if (check(x, y)) del(x, y); break;
+            default: cout << "Unrecognized instructions." << endl << "Press any key to continue." << endl; getch(); break;
+        }
+
+        draw();
+    }
+}
+
+void open(int x, int y) {
+    if (m[x][y]) gameover();
+    if (f[x][y]) {cout << "Can't open this cell.Flags on." << endl << "Press any key to continue." << endl; getch(); return;}
+    vis[x][y] = true;
+}
+
+void flag(int x, int y) {
+    if (!flags) {cout << "You use out of flags.Try to remove some of them." << endl << "Press any key to continue." << endl; getch(); return;}
+    if (vis[x][y]) {cout << "Can't use flag.You've visited this cell." << endl << "Press any key to continue." << endl; getch(); return;}
+    f[x][y] = true; flags--;
+
+    win();
+}
+
+void del(int x, int y) {
+    if (!f[x][y]) {cout << "This cell doesn't have flag." << endl << "Press any key to continue." << endl; getch(); return;}
+    f[x][y] = false; flags++;
 }
